f147266f8e7d3bd654a885f35cbd71ee86c29f73
Anabra <peter.d.podlovics@gmail.com>
2022-07-09 23:38:31 +0200

    refactor retry mechanism

--- src/wallstreetbets_crawler.clj
+++ src/wallstreetbets_crawler.clj
@@ -1,7 +1,6 @@
 (ns wallstreetbets-crawler
   (:require
    [clj-http.client :as client]
-   [slingshot.slingshot :refer [try+]]
    [wallstreetbets-crawler.auth :as auth]
    [wallstreetbets-crawler.log :as log]
    [wallstreetbets-crawler.util :as util]))
@@ -10,23 +9,32 @@
   [s]
   (str (subs s 0 100) "...<truncated>"))
 
+(defn retry-fn-with-wait
+  [failed? num-tries nullary-fn]
+  (let [og-result (nullary-fn)]
+    (loop [remaining-tries (dec num-tries)
+           wait-time       1000
+           result          og-result]
+      (cond
+        (<= remaining-tries 0)
+        (log/error! "Maximum number of retries reached, returning nil")
+
+        (failed? result)
+        (do
+          (log/info! (str "Expression failed, retrying after " (/ wait-time 1000) " seconds"))
+          (Thread/sleep wait-time)
+          (recur (dec remaining-tries)
+                 (* 2 wait-time)
+                 (nullary-fn)))
+
+        :else result))))
+
 (defmacro with-retries
   "Try evaluationg an expression, and retry if the result failed.
   Returns nil if none of the attempts succeeded.
   "
   [failed? num-tries expr]
-  (let [wait-times (->> (range 0 (dec num-tries))
-                        (map #(Math/pow 2 %))
-                        (map (partial * 1000)))
-        retries (for [t wait-times]
-                  `(do
-                     (log/info! ~(str "Expression failed, retrying after " (Math/round (/ t 1000)) " seconds"))
-                     (Thread/sleep ~t)
-                     ~expr))]
-    `(some (complement ~failed?)
-           [~expr
-            ~@retries
-            (log/error! "Maximum number of retries reached, returning nil")])))
+  `(retry-fn-with-wait ~failed? ~num-tries (fn [] ~expr)))
 
 (defn log-non-200-error
   [{:keys [url params] :as _request}
@@ -58,20 +66,24 @@
 
 (defn get-with-handlers!
   [url params {:keys [default] :as handlers}]
-  (let [{:keys [status] :as response} (client/get url params)
-        request {:url url
-                 :params params}
+  (let [params-no-ex (assoc params :throw-exceptions false)
+        {:keys [status] :as response} (client/get url params-no-ex)
+        request {:url    url
+                 :params params-no-ex}
         handler (get handlers status default)]
-    (handler request response)))
+    (case status
+      200 response
+      (handler request response))))
 
 (defn get-with-retries!
   [url params]
   (let [{:keys [body]}
         (with-retries #(not= 200 (:status %)) 3
           (get-with-handlers!
-           url params {200 identity
-                       401 refresh-access-token+log-401-error
-                       :default log-non-200-error}))]
+           url
+           (-> params auth/with-user-agent auth/update-access-token)
+           {401      refresh-access-token+log-401-error
+            :default log-non-200-error}))]
     (try
       (util/parse-json body)
       (catch Exception e
@@ -87,9 +99,7 @@
   (let [url "https://oauth.reddit.com/r/wallstreetbets/new"
         params (->> {:query-params
                      {:limit 1}
-                     :accept :json}
-                    auth/with-user-agent
-                    auth/with-access-token)]
+                     :accept :json})]
     (get-with-retries! url params)))
 
 (defn extract-article-fields
@@ -115,7 +125,7 @@
 
 (comment
 
-(get-articles!)
+  (get-articles!)
 
   "plan of attack
 - get top 100 articles
