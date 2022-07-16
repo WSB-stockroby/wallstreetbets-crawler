0eee42bf0d18d4bf8d71eaf05b7c5bdc16289ecf
Anabra <peter.d.podlovics@gmail.com>
2022-07-16 22:21:48 +0200

    define producer interface

--- src/wallstreetbets_crawler/kafka.clj
+++ src/wallstreetbets_crawler/kafka.clj
@@ -6,9 +6,9 @@
    [wallstreetbets-crawler.log :as log])
   (:import
    (java.util Properties)
-   (org.apache.kafka.clients.admin AdminClient NewTopic)
-   (org.apache.kafka.clients.producer Callback KafkaProducer ProducerConfig ProducerRecord)
-   (org.apache.kafka.common.errors TopicExistsException)))
+   (org.apache.kafka.clients.producer Callback KafkaProducer ProducerRecord)))
+
+;; NOTE: based on https://github.com/confluentinc/examples/blob/a5fbc1920277a8177606b07aa8341d24297084b6/clients/cloud/clojure/src/io/confluent/examples/clients/clj/producer.clj#L1
 
 (defn load-kafka-producer-config!
   []
@@ -34,18 +34,28 @@
    :offset    (. record-metadata offset)
    :timestamp (. record-metadata timestamp)})
 
-(defn produce!
-  [config topic]
+(defn with-producer
+  [config f]
   (with-open [producer (KafkaProducer. (config=>properties config))]
-    (doseq [i (range 0 3)]
-      (let [callback (reify Callback
-                       (onCompletion [_this _record-meta ex]
-                         (when ex (log/warn! "Record delivery failure"
-                                             {:ex ex}))))]
-        (.send
-         producer
-         (make-record topic i {:count i})
-         callback)))
+    (f producer)
     (.flush producer)))
 
-(produce! (config=>properties kafka-producer-config) "hello_world")
+(defn send!
+  [producer topic k v]
+  (let [callback (reify Callback
+                   (onCompletion [_this _record-meta ex]
+                     (when ex (log/warn! "Record delivery failure"
+                                         {:ex ex}))))]
+    (.send
+     producer
+     (make-record topic k v)
+     callback)))
+
+(comment
+
+ (with-producer (load-kafka-producer-config!)
+   (fn [p]
+     (doseq [i (range 0 3)]
+       (send! p "whatever" i {:count i}))))
+
+ "asd")
